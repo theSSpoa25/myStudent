2090b264c38d7829dbb8229e39d991e2546729e8
Egert Gjyla <egertgjyla@outlook.com>
2021-01-02 16:36:42 +0100

    initial commit

---
+++ bachelorThesis/src/main/java/com/egertgjyla/bachelorThesis/BachelorThesisApplication.java
@@ -0,0 +1,13 @@
+package com.egertgjyla.bachelorThesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorThesisApplication.class, args);
+	}
+
+}
---
+++ bachelorThesis/src/test/java/com/egertgjyla/bachelorThesis/BachelorThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egertgjyla.bachelorThesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
